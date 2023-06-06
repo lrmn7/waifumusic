ecbbd65bd42dba2b9720f470cfae27d448f8502a
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-06-06 15:32:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 require('./Structures/WaifuMusic');
+const express = require('express');
+const app = express();
+
+// Set up routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
